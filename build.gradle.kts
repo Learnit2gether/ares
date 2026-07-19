830ab7730879447cd494ba4af08ca10503c975d9
gaurav <gauravc258@gmail.com>
2026-07-19 14:25:23 +0530

    change source set

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,7 @@ plugins {
 
 android {
     namespace = "com.cp.ares"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 24
